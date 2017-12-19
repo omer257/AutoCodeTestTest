d2610b0635e5fb4ec8569749534c876665aaef41
Anna W <aweinmacher@gmail.com>
2017-12-19 12:13:22 +0200

    extension 2

--- main.js
+++ main.js
@@ -24,8 +24,23 @@ var coffeeShop = {
   }
 }
 
+coffeeShop.money = 100;
+coffeeShop.buySupplies = function (amountOfBeans) {
+  while(this.money >= amountOfBeans*2) {
+    this.beans += amountOfBeans;
+    this.money -= amountOfBeans*2;
+    alert("You've bought " + amountOfBeans + " beans. Money balance: " + this.money);
+    return;
+  }
+  alert("Mind the price 2 shekels per bean. Your balance is not enough: " + this.money);
+}
+
 coffeeShop.makeDrink("latte");
 coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+
+coffeeShop.buySupplies(10);
+coffeeShop.buySupplies(55);
+console.log(coffeeShop);

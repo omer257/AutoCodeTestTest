1b283788fd73508848c6cc4ec7aa0c0a17ced186
omi <omi@omis-MacBook-Pro.local>
2017-12-20 16:41:44 +0200

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var coffeeShop = {
   beans: 40,
-  money: 100,
 
   drinkRequirements: {
     latte: { beanRequirement: 10, price: 21 },
@@ -10,37 +9,45 @@ var coffeeShop = {
   },
 
   makeDrink: function (drinkType) {
-    if (!this.drinkRequirements.hasOwnProperty(drinkType)) {
-      alert("Sorry, we don't make " + drinkType);
-    } else if (this.beans < this.drinkRequirements[drinkType].beanRequirement) {
-      alert("Sorry, we're all out of beans!");
-    } else {
-      this.beans -= this.drinkRequirements[drinkType].beanRequirement;
-      alert("Enjoy your " + drinkType + ", we have " + this.beans + "beans more.");
+    for (drink in this.drinkRequirements) {
+      if (drink === drinkType) {
+        while (this.beans >= this.drinkRequirements[drinkType].beanRequirement) {
+          this.beans -= this.drinkRequirements[drinkType].beanRequirement;
+          alert("Enjoy your " + drinkType + ", we have " + this.beans + "beans more.");
+          return;
+        }
+        alert("Sorry, we're all out of beans!");
+        return;
+      }
     }
-  },
+    alert("Sorry, we don't make " + drinkType);
+  }
+}
 
-  buySupplies: function (amountOfBeans) {
-    if (this.money >= amountOfBeans * 2) {
-      this.beans += amountOfBeans;
-      this.money -= amountOfBeans * 2;
-      alert("You've bought " + amountOfBeans + " beans. Money balance: " + this.money);
+coffeeShop.money = 100;
+coffeeShop.buySupplies = function (amountOfBeans) {
+  while (this.money >= amountOfBeans * 2) {
+    this.beans += amountOfBeans;
+    this.money -= amountOfBeans * 2;
+    alert("You've bought " + amountOfBeans + " beans. Money balance: " + this.money);
+    return;
+  }
+  alert("Mind the price 2 shekels per bean. Your balance is not enough: " + this.money);
+}
+
+coffeeShop.buyDrink = function (drinkType) {
+  for (drink in this.drinkRequirements) {
+    if (drink === drinkType && this.beans >= this.drinkRequirements[drinkType].beanRequirement) {
+    this.money += this.drinkRequirements[drinkType].price;
+    this.makeDrink(drinkType);
+    alert("Money balance: " + this.money + "; Bean balance: " + this.beans);
+    return;
+    } else if (drink === drinkType) {
+      alert("Sorry, we're all out of beans!");
       return;
     }
-    alert("Mind the price 2 shekels per bean. Your balance is not enough: " + this.money);
-  },
-
-  buyDrink: function (drinkType) {
-    if (!drink in this.drinkRequirements) {
-      alert("Sorry, we don't make " + drinkType);
-    } else if (this.beans < this.drinkRequirements[drinkType].beanRequirement) {
-      alert("Sorry, we're all out of beans!");
-    } else {
-      this.money += this.drinkRequirements[drinkType].price;
-      this.makeDrink(drinkType);
-      alert("Money balance: " + this.money + "; Bean balance: " + this.beans);
-    }
   }
+  alert("Sorry, we don't make " + drinkType);
 }
 
 coffeeShop.makeDrink("latte");
@@ -51,6 +58,7 @@ coffeeShop.makeDrink("frenchPress");
 
 coffeeShop.buySupplies(10);
 coffeeShop.buySupplies(55);
+console.log(coffeeShop);
 
 coffeeShop.buyDrink("latte");
 coffeeShop.buyDrink("americano");
